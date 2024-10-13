69777e7011671e5eba290e43602e257a3886e063
kiruthi <priyakiruthi21@gmail.com>
2024-10-13 21:45:00 +0530

    fix router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import BrowserRouter
 import UserForm from './components/UserForm/UserForm';
 import AdminDashboard from './components/AdminDashboard/AdminDashboard';
 import Login from './components/Login/Login';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<UserForm />} />
-      <Route path="/admin-login" element={<Login />} />
-      <Route path="/admin" element={<AdminDashboard />} />
-      <Route path="*" element={<h1>Not Found</h1>} />
-    </Routes>
+    <Router> {/* Wrap Routes in BrowserRouter */}
+      <Routes>
+        <Route path="/" element={<UserForm />} />
+        <Route path="/admin-login" element={<Login />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </Router>
   );
 }
 

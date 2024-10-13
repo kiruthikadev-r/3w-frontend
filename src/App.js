4486926ebb7352a9f5892c03fe40a8a05dff1956
kiruthi <priyakiruthi21@gmail.com>
2024-10-13 21:27:31 +0530

    fixed api

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<UserForm />} />
-        <Route path="/admin" element={<AdminDashboard />} />
         <Route exact path="/login" element={<Login />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route exact path="/" element={<UserForm />} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
     </Router>

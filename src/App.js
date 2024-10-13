23820f0e13e1e434065cbce2d75aa9d8df91a3b0
kiruthi <priyakiruthi21@gmail.com>
2024-10-13 21:31:24 +0530

    fixed route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import UserForm from './components/UserForm/UserForm';
 import AdminDashboard from './components/AdminDashboard/AdminDashboard';
 import Login from './components/Login/Login';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/login" element={<Login />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route exact path="/" element={<UserForm />} />
-        <Route path="*" element={<h1>Not Found</h1>} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<UserForm />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/admin" element={<AdminDashboard />} />
+      <Route path="*" element={<h1>Not Found</h1>} />
+    </Routes>
   );
 }
 
